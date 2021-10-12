57e5040fb7a979e3b0b61d59acf3e3fdfa8e8640
David Ezequiel Resendiz Rodriguez <89091725+David-Uabc@users.noreply.github.com>
2021-10-12 16:06:21 -0700

    Add files via upload

---
+++ practica6/src/practica6/Main.java
@@ -0,0 +1,13 @@
+package practica6;
+
+public class Main {
+	
+	    public static void main(String[] args){
+        Torneo torneo  = new Torneo(" NBA Mexico", "MEXICO", 5, 12,25);// relleno con la liga(yo la puse como el torneo tambien) region asi como los quipos que estaran y ademas
+        //los partidos judados y faltantes
+        torneo.addEquipos();
+        System.out.println("--------********--------********--------********--------");
+        torneo.datosTorneo();
+    }
+	
+}
